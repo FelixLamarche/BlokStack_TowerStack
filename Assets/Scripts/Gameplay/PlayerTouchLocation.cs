7f46f231853388f895b4f598ee28b1155d4a194a
FelixLamarche <felixlamarche28@gmail.com>
2022-06-03 21:44:23 -0400

    Cleanup of camera fix
    Quickly cleaned up the scene and ajusted some gameplay values

--- Assets/Scripts/Gameplay/PlayerTouchLocation.cs
+++ Assets/Scripts/Gameplay/PlayerTouchLocation.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class PlayerTouchLocation : MonoBehaviour
 {
-    [SerializeField]
-    GameObject[] test;
-
     GameCamera gameCamera;
     CircleCollider2D touchCollider;
 
@@ -20,14 +17,6 @@ public class PlayerTouchLocation : MonoBehaviour
         touchCollider.enabled = GameInput.IsTouchingScreen;
         if(GameInput.IsTouchingScreen)
         {
-            // TODO GO FIX Calculate EDGE OF SCREEN To ACCOUNT FOR TOP/BOTTOM DISCREPANCY
-            float yTop = gameCamera.CalculateEdgeOfScreenPosition(0, VerticalDirection.above);
-            float tBot = gameCamera.CalculateEdgeOfScreenPosition(0, VerticalDirection.below);
-            Debug.Log("Top: " + yTop);
-            Debug.Log("Bot: " + tBot);
-            test[0].transform.position = new Vector3(0, yTop, 0);
-            test[1].transform.position = new Vector3(0, tBot, 0);
-
             Vector3 screenPosWithDepth = new Vector3(GameInput.TouchPosition.x, GameInput.TouchPosition.y, transform.position.z);
             Vector2 pos2D = gameCamera.ScreenPositionToWorldPoint2D(screenPosWithDepth);
             transform.position = new Vector3(pos2D.x, pos2D.y, transform.position.z);
@@ -36,13 +25,14 @@ public class PlayerTouchLocation : MonoBehaviour
 
     void OnTriggerEnter2D(Collider2D collider2D){
         if(collider2D.tag == "PlayerObstacle"){
+            Debug.Log("STOP");
             FindObjectOfType<GameplayManager>().StopGame();
         }
     }
 
     void OnDrawGizmos()
     {
-        float radius = touchCollider == null ? 0.5f : touchCollider.radius;
+        float radius = touchCollider == null ? 0.25f : touchCollider.radius;
         Gizmos.DrawWireSphere(transform.position, radius);
     }
 }
